7e9b405fede4962cf5d051c69bef7b73cddb8997
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-01-22 17:02:53 +0100

    implemented filtering for rooms and zones

--- IfcSpaceZoneBoundaries/Command.cs
+++ IfcSpaceZoneBoundaries/Command.cs
@@ -14,6 +14,30 @@ namespace IfcSpaceZoneBoundaries
   [Transaction( TransactionMode.ReadOnly )]
   public class Command : IExternalCommand
   {
+    const string _pname_export_as = "IfcexportAs";
+    const string _export_as_room = "IfcSpace.INTERNAL";
+    const string _export_as_zone = "Ifczone";
+
+    static string GetStringParamValue( 
+      Element e, 
+      string pname )
+    {
+      IList<Parameter> ps = e.GetParameters( pname );
+
+      int n = ps.Count;
+
+      if( 1 < n )
+      {
+        throw new ArgumentException( 
+          "expected maximum one parameter named "
+            + pname );
+      }
+
+      return ( 1 == n )
+        ? ps[0].AsString()
+        : null;
+    }
+
     public Result Execute(
       ExternalCommandData commandData,
       ref string message,
@@ -29,11 +53,19 @@ namespace IfcSpaceZoneBoundaries
 
       FilteredElementCollector col
         = new FilteredElementCollector( doc )
-          .OfClass( typeof( DirectShape ) );
+          .OfClass( typeof( DirectShape ) )
+          .OfCategory( BuiltInCategory.OST_GenericModel );
 
       foreach( Element e in col )
       {
-        Debug.Print( e.Name );
+        string export_as = GetStringParamValue( 
+          e, _pname_export_as );
+
+        if( export_as.Equals( _export_as_room )
+          || export_as.Equals( _export_as_zone ) )
+        {
+          Debug.Print( e.Name );
+        }
       }
       return Result.Succeeded;
     }
