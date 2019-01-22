e0cc28e7a0abc8d8a4c45d94323c8b1155135df2
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-01-22 17:06:40 +0100

    implemented RoomZoneExporter class

--- IfcSpaceZoneBoundaries/Command.cs
+++ IfcSpaceZoneBoundaries/Command.cs
@@ -14,30 +14,6 @@ namespace IfcSpaceZoneBoundaries
   [Transaction( TransactionMode.ReadOnly )]
   public class Command : IExternalCommand
   {
-    const string _pname_export_as = "IfcexportAs";
-    const string _export_as_room = "IfcSpace.INTERNAL";
-    const string _export_as_zone = "Ifczone";
-
-    static string GetStringParamValue( 
-      Element e, 
-      string pname )
-    {
-      IList<Parameter> ps = e.GetParameters( pname );
-
-      int n = ps.Count;
-
-      if( 1 < n )
-      {
-        throw new ArgumentException( 
-          "expected maximum one parameter named "
-            + pname );
-      }
-
-      return ( 1 == n )
-        ? ps[0].AsString()
-        : null;
-    }
-
     public Result Execute(
       ExternalCommandData commandData,
       ref string message,
@@ -48,25 +24,6 @@ namespace IfcSpaceZoneBoundaries
       Application app = uiapp.Application;
       Document doc = uidoc.Document;
 
-      // IFC room and zones are represented by
-      // DirectShape elements.
-
-      FilteredElementCollector col
-        = new FilteredElementCollector( doc )
-          .OfClass( typeof( DirectShape ) )
-          .OfCategory( BuiltInCategory.OST_GenericModel );
-
-      foreach( Element e in col )
-      {
-        string export_as = GetStringParamValue( 
-          e, _pname_export_as );
-
-        if( export_as.Equals( _export_as_room )
-          || export_as.Equals( _export_as_zone ) )
-        {
-          Debug.Print( e.Name );
-        }
-      }
       return Result.Succeeded;
     }
   }
---
+++ IfcSpaceZoneBoundaries/RoomZoneExporter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Autodesk.Revit.DB;
+
+namespace IfcSpaceZoneBoundaries
+{
+  class RoomZoneExporter
+  {
+    const string _pname_export_as = "IfcexportAs";
+    const string _export_as_room = "IfcSpace.INTERNAL";
+    const string _export_as_zone = "Ifczone";
+
+    static string GetStringParamValue(
+      Element e,
+      string pname )
+    {
+      IList<Parameter> ps = e.GetParameters( pname );
+
+      int n = ps.Count;
+
+      if( 1 < n )
+      {
+        throw new ArgumentException(
+          "expected maximum one parameter named "
+            + pname );
+      }
+
+      return ( 1 == n )
+        ? ps[0].AsString()
+        : null;
+    }
+
+    public RoomZoneExporter( Document doc )
+    {
+      // IFC room and zones are represented by
+      // DirectShape elements.
+
+      FilteredElementCollector col
+        = new FilteredElementCollector( doc )
+          .OfClass( typeof( DirectShape ) )
+          .OfCategory( BuiltInCategory.OST_GenericModel );
+
+      foreach( Element e in col )
+      {
+        string export_as = GetStringParamValue(
+          e, _pname_export_as );
+
+        if( export_as.Equals( _export_as_room )
+          || export_as.Equals( _export_as_zone ) )
+        {
+          Debug.Print( e.Name );
+        }
+      }
+    }
+  }
+}
