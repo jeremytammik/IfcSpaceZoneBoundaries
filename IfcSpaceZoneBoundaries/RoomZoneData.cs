2c1dff28884e5d996746c1c84fc0ff6b80f8a32c
Jeremy Tammik <jeremy.tammik@autodesk.com>
2019-01-22 17:31:49 +0100

    populate IFC properties in RoomZoneData constructor

--- IfcSpaceZoneBoundaries/RoomZoneData.cs
+++ IfcSpaceZoneBoundaries/RoomZoneData.cs
@@ -1,4 +1,6 @@
-﻿using Autodesk.Revit.DB;
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.Revit.DB;
 
 namespace IfcSpaceZoneBoundaries
 {
@@ -14,9 +16,64 @@ namespace IfcSpaceZoneBoundaries
     public string Pset;
     public string Boundary;
 
+    public bool IsRoomOrZone
+    {
+      get { return null != Space_or_Zone; }
+    }
+
+    const string _pname_export_as = "IfcExportAs";
+    const string _pname_guid = "IfcGUID";
+    const string _pname_name = "IfcName";
+    const string _pname_layer = "IfcPresentationLayer";
+    const string _pname_pset = "IfcPropertySetList";
+    const string _pname_zone = "IfcZone";
+    const string _export_as_room = "IfcSpace.INTERNAL";
+    const string _export_as_zone = "Ifczone";
+
+    static string GetStringParamValue(
+      Element e,
+      string pname )
+    {
+      IList<Parameter> ps = e.GetParameters( pname );
+
+      int n = ps.Count;
+
+      if( 1 < n )
+      {
+        throw new ArgumentException(
+          "expected maximum one parameter named "
+            + pname );
+      }
+
+      return ( 1 == n )
+        ? ps[0].AsString()
+        : null;
+    }
+
+
     public RoomZoneData( Element e )
     {
+      string export_as = GetStringParamValue(
+        e, _pname_export_as );
 
+      if( export_as.Equals( _export_as_room ) )
+      {
+        Space_or_Zone = "S";
+      }
+      else if( export_as.Equals( _export_as_zone ) )
+      {
+        Space_or_Zone = "Z";
+      }
+
+      if(IsRoomOrZone)
+      {
+        GUID = GetStringParamValue( e, _pname_guid );
+        Name = GetStringParamValue( e, _pname_name );
+        Zone = GetStringParamValue( e, _pname_zone );
+        Layer = GetStringParamValue( e, _pname_layer );
+        Pset = GetStringParamValue( e, _pname_pset );
+        Boundary = null;
+      }
     }
 
     public string AsString()
--- IfcSpaceZoneBoundaries/RoomZoneExporter.cs
+++ IfcSpaceZoneBoundaries/RoomZoneExporter.cs
@@ -1,36 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Autodesk.Revit.DB;
 
 namespace IfcSpaceZoneBoundaries
 {
   class RoomZoneExporter
   {
-    const string _pname_export_as = "IfcexportAs";
-    const string _export_as_room = "IfcSpace.INTERNAL";
-    const string _export_as_zone = "Ifczone";
-
-    static string GetStringParamValue(
-      Element e,
-      string pname )
-    {
-      IList<Parameter> ps = e.GetParameters( pname );
-
-      int n = ps.Count;
-
-      if( 1 < n )
-      {
-        throw new ArgumentException(
-          "expected maximum one parameter named "
-            + pname );
-      }
-
-      return ( 1 == n )
-        ? ps[0].AsString()
-        : null;
-    }
-
     public RoomZoneExporter( Document doc )
     {
       // IFC room and zones are represented by
@@ -43,13 +17,9 @@ namespace IfcSpaceZoneBoundaries
 
       foreach( Element e in col )
       {
-        string export_as = GetStringParamValue(
-          e, _pname_export_as );
-
-        if( export_as.Equals( _export_as_room )
-          || export_as.Equals( _export_as_zone ) )
-        {
-          RoomZoneData d = new RoomZoneData( e );
+        RoomZoneData d = new RoomZoneData( e );
+        if( d.IsRoomOrZone )
+        { 
           Debug.Print( d.AsString() );
         }
       }
